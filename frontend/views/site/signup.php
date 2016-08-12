d7e1622a2296e7367f7890d1be1c93c707caa5c8
Adsavin <adsavin@live.com>
2016-08-12 10:45:05 +0700

    init

---
+++ frontend/views/site/signup.php
@@ -0,0 +1,39 @@
+<?php
+/* @var $this yii\web\View */
+/* @var $form yii\bootstrap\ActiveForm */
+/* @var $model \frontend\models\SignupForm */
+
+use yii\helpers\Html;
+use yii\bootstrap\ActiveForm;
+
+$this->title = 'Signup';
+$this->params['breadcrumbs'][] = $this->title;
+?>
+<div class="site-signup">
+    <div class="row">
+        <div class="col-lg-6">
+            <div class="panel panel-primary">
+                <div class="panel panel-heading">
+                    <h1><?= Html::encode($this->title) ?></h1>
+                </div>
+                <div class="panel panel-body">
+                    <p>Please fill out the following fields to signup:</p>
+                    <?php $form = ActiveForm::begin(['id' => 'form-signup']); ?>
+
+                    <?= $form->field($model, 'username')->textInput(['autofocus' => true]) ?>
+
+                    <?= $form->field($model, 'email') ?>
+
+                    <?= $form->field($model, 'password')->passwordInput() ?>
+
+                    <div class="form-group">
+                        <?= Html::submitButton('Signup', ['class' => 'btn btn-primary', 'name' => 'signup-button']) ?>
+                    </div>
+
+                    <?php ActiveForm::end(); ?>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</div>
